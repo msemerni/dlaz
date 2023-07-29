3eece172ae83197774eef3e73787a1785d0e3e2f
msemerni <msemerni@gmail.com>
2023-07-29 22:06:10 +0300

    clean comments

--- js/lang.js
+++ js/lang.js
@@ -59,25 +59,21 @@ const langArr = {
       de: "Die Laser-Haarentfernung ist der Schlüssel, um Primärpatienten in jeden Schönheitssalon zu locken",
       sk: "Laserové odstránenie chĺpkov je kľúčom k prilákaniu primárnych pacientov do akéhokoľvek salónu krásy",
     },
-
     left__laser_1_price_text: {
       en: "Price:",
       de: "Preis:",
       sk: "Cena:",
     },
-
     left__laser_1_price: {
       en: "€ 120",
       de: "€ 120",
       sk: "€ 120",
     },
-
     left__laser_1_charact_header: {
       en: "Specifications:",
       de: "Spezifikationen:",
       sk: "Technické údaje:",
     },
-
     left__laser_1_charact_1: {
       en: "12 bar 800 w power handle",
       de: "12 bar 800 W Kraftgriff",
@@ -103,25 +99,21 @@ const langArr = {
       de: "LED-Bildschirmanzeige 12.6",
       sk: "LED displej 12.6",
     },
-
     right__laser_2_price_text: {
       en: "Price:",
       de: "Preis:",
       sk: "Cena:",
     },
-
     right__laser_2_price: {
       en: "€ 130",
       de: "€ 130",
       sk: "€ 130",
     },
-
     right__laser_2_charact_header: {
       en: "Specifications:",
       de: "Spezifikationen:",
       sk: "Technické údaje:",
     },
-
     right__laser_2_charact_1: {
       en: "12 bar 1200 w power handle",
       de: "12 bar 1200 W Kraftgriff",
@@ -147,13 +139,11 @@ const langArr = {
       de: "LED-Bildschirmanzeige 14\"",
       sk: "LED displej 14\"",
     },
-
     left_p2: {
       en: "High level of laser hair removal services allows you to wait for subsequent visits of patients for other types of cosmetic procedures.",
       de: "Hochwertige Dienstleistungen zur Laser-Haarentfernung ermöglicht es Ihnen, auf spätere Patientenbesuche zu warten für andere Arten kosmetischer Eingriffe.",
       sk: "Služby laserovej epilácie na vysokej úrovni umožňuje čakať na následné návštevy pacientov pre iné typy kozmetických procedúr.",
     },
-
     right_p1: {
       en: "Laser hair removal is one of the most popular cosmetology searches.",
       de: "Die Laser-Haarentfernung ist eine der beliebtesten Suchanfragen in der Kosmetikbranche.",
@@ -164,7 +154,6 @@ const langArr = {
       de: "JETZT die Ausrüstung mieten!",
       sk: "Požičajte si vybavenie TERAZ!",
     },
-
   },
 
 
@@ -189,7 +178,6 @@ const langArr = {
       de: "Bei der Anmietung von D-Laz-Geräten müssen keine Mittel investiert werden, die für Werbung und Marketing verwendet werden können!\nSchneller Versand der Geräte und Anschluss bei Ihnen vor Ort",
       sk: "Pri prenájme zariadení D-Laz nie je potrebné investovať finančné prostriedky využiteľné v reklame a marketingu!\nRýchle dodanie zariadení a pripojenie u vás",
     },
-    
     card_2_title: {
       en: "Low risks",
       de: "Geringe risiken",
@@ -200,7 +188,6 @@ const langArr = {
       de: "Wenn Sie ein Gerät mieten, riskieren Sie keinen Verlust, denn was zahlen Sie erst, wenn Sie es selbst verdienen.\nDie Amortisation des Geräts erfolgt bei nur einem Patienten pro Tag.\nWir bieten wie kein anderer die Wahl zwischen Bezahlung für die Miete von Geräten:\n- Wir bieten Ihnen die Möglichkeit, bei Ausbrüchen zu bezahlen.",
       sk: "Ak si prístroj prenajmete, neriskujete stratu, pretože čo platíte, len keď si zarobíte.\nSplatnosť prístroja dosiahnete len s jedným pacientom denne.\nAko nikto iný poskytujeme mám na výber platba za prenájom zariadení:\n- Dáme vám možnosť platiť za ohniská.",
     },
-
     card_3_title: {
       en: "Warranty for all devices",
       de: "Garantie für alle Geräte",
@@ -211,7 +198,6 @@ const langArr = {
       de: "Die Garantie gilt für die gesamte Mietdauer und die kostenlose Wartung der Geräte.\n Im Garantiefall beheben wir das Problem umgehend oder stellen Ihnen ein Ersatzgerät zur Verfügung, das Ihnen die Möglichkeit gibt, wie gewohnt zu arbeiten und zu arbeiten Warten Sie nicht auf eine Lösung des Problems im Garantiefall.\nSie haben keine Ausfallzeiten und die Kunden sind immer zufrieden.",
       sk: "Záruka je poskytovaná na celú dobu prenájmu a bezplatnú údržbu zariadení.\n V prípade záručného prípadu problém ihneď odstránime alebo vám poskytneme náhradné zariadenie, čo vám dáva možnosť pracovať ako obvykle a nečakajte na riešenie problému so záručným prípadom.\nNemáte žiadne prestoje a zákazníci sú vždy spokojní.",
     },
-
     card_4_title: {
       en: "Service maintenance",
       de: "Service-Wartung",
@@ -222,7 +208,6 @@ const langArr = {
       de: "Wir sind immer bereit, technische Probleme umgehend zu lösen.",
       sk: "Sme vždy pripravení promptne vyriešiť akékoľvek technické problémy.",
     },
-
     card_5_title: {
       en: "Low investment",
       de: "Geringe Investition",
@@ -233,7 +218,6 @@ const langArr = {
       de: "",
       sk: "",
     },
-
     card_6_title: {
       en: "Flexible rental conditions and the possibility of buying",
       de: "Flexible Mietkonditionen und Kaufmöglichkeit",
@@ -244,7 +228,6 @@ const langArr = {
       de: "",
       sk: "",
     },
-
     card_7_title: {
       en: "Support",
       de: "Unterstützung",
@@ -255,7 +238,6 @@ const langArr = {
       de: "",
       sk: "",
     },
-
     card_8_title: {
       en: "Certification",
       de: "Zertifizierung",
@@ -266,7 +248,6 @@ const langArr = {
       de: "",
       sk: "",
     },
-
     card_9_title: {
       en: "Marketing package",
       de: "Marketingpaket",
@@ -286,7 +267,6 @@ const langArr = {
       de: "Foto / Video von Models",
       sk: "Foto / video podľa modelov",
     },
-
     header2: {
       en: "Our lasers have a simple and intuitive interface.\n Interface Language: Russian, English, German, Spanish.\n See for yourself!\n Our hair removal laser in action.\n Watch the procedure video and find out how easy it is to use D-Laz.",
       de: "Unsere Laser verfügen über eine einfache und intuitive Benutzeroberfläche.\n Benutzeroberflächensprache: Russisch, Englisch, Deutsch, Spanisch.\n Überzeugen Sie sich selbst!\n Unser Haarentfernungslaser in Aktion.\n Sehen Sie sich das Verfahrensvideo an und finden Sie heraus, wie einfach es ist D-Laz verwenden.",
@@ -301,13 +281,11 @@ const langArr = {
       de: "Kontakte",
       sk: "Kontakty",
     },
-
     feedback: {
       en: "Write to us",
       de: "Schreib uns",
       sk: "Napíšte nám",
     },
-
     author_before: {
       en: "written by",
       de: "geschrieben von",
