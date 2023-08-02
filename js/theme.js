cb91c3dd8c696264f8dc5073499db2776db27afe
msemerni <msemerni@gmail.com>
2023-08-02 17:00:08 +0300

    First release

--- js/theme.js
+++ js/theme.js
@@ -3,34 +3,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
   let currentMode = localStorage.getItem('mode');
 
-  // Set default mode to light if not stored in local storage
   if (!currentMode) {
     currentMode = 'light';
     localStorage.setItem('mode', currentMode);
   }
 
-  // Apply the current mode
   applyMode(currentMode);
 
-
-  // Event listener for toggle button click
   toggleButton.addEventListener('click', function () {
     currentMode = currentMode === 'dark' ? 'light' : 'dark';
-    localStorage.setItem('mode', currentMode); // Сохраняем выбранный режим в Local Storage
+    localStorage.setItem('mode', currentMode);
     applyMode(currentMode);
   });
 
   function applyMode(mode) {
     document.documentElement.setAttribute('data-mode', mode);
 
-    // Toggle button icons
     const lightToggleIcon = toggleButton.querySelector('.lightToggleIcon');
     const darkToggleIcon = toggleButton.querySelector('.darkToggleIcon');
 
     lightToggleIcon.style.display = mode === 'light' ? 'block' : 'none';
     darkToggleIcon.style.display = mode === 'dark' ? 'block' : 'none';
 
-    // Update the "select" element background
     const selectElement = document.querySelector('select');
     selectElement.classList.toggle('dark-mode', mode === 'dark');
     selectElement.classList.toggle('light-mode', mode === 'light');
