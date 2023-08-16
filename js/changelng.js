83c8d64df85195b37739b476a7ca2b049e8e69a0
msemerni <msemerni@gmail.com>
2023-08-16 22:53:36 +0300

    seo add

--- js/changelng.js
+++ js/changelng.js
@@ -1,5 +1,5 @@
-const URL = "http://127.0.0.1:5500";
-// const URL = "https://dlaz.online";
+// const URL = "http://127.0.0.1:5500";
+const URL = "https://dlaz.online";
 
 let selectLang = document.querySelector('.select-lang');
 
@@ -19,5 +19,5 @@ function changeLanguage() {
   let lang = selectLang.value;
   selectLang.value = lang;
   localStorage.setItem("lang", lang);
-  location.href = `${URL}/${lang}`;
+  location.href = `${URL}/${lang}/`;
 }
